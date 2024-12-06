245804fe121a95d775ac8aeb3d96168384f1deaf
Jessica Moore <jessica.cmoore1@gmail.com>
2024-12-06 17:45:18 -0500

    :lipstick: Desktop styles

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import mobileImg from '../assets/background-pattern-mobile.svg';
+import desktopImg from '../assets/background-pattern-desktop.svg';
 
 type ContainerProps = React.HtmlHTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ type ContainerProps = React.HtmlHTMLAttributes<HTMLDivElement> & {
 
 const StyledWrapper = styled.div`
   min-height: 100vh;
-  padding-block: 10rem;
+  padding-block: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,6 +17,11 @@ const StyledWrapper = styled.div`
     `url("${mobileImg}"), linear-gradient(to right, ${theme.colors.secondary}, ${theme.colors.secondary})`};
   background-repeat: no-repeat;
   background-size: contain;
+
+  @media (min-width: 768px) {
+    background-image: ${({ theme }) =>
+      `url("${desktopImg}"), linear-gradient(to right, ${theme.colors.secondary}, ${theme.colors.secondary})`};
+  }
 `;
 
 const StyledContainer = styled.div`
